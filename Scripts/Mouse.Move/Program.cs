440af24ecbf4eda0c46f5159a7c6d7a40f201d07
gis2all <1262057294@qq.com>
2020-04-07 21:05:59 +0800

    Update code

--- Scripts/Mouse.Move/Program.cs
+++ Scripts/Mouse.Move/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Threading;
 using System.Windows;
 using Utils;
+using Utils.Core;
 
 namespace Mouse.Move
 {
@@ -23,15 +24,35 @@ namespace Mouse.Move
                 if (i % 2 == 0)
                 {
                     Win32Utils.MouseEvent(Win32Constants.MOUSEEVENTF_MOVE, 2, 0, 0, 0);
+
+
                 }
                 else
                 {
                     Win32Utils.MouseEvent(Win32Constants.MOUSEEVENTF_MOVE, -2, 0, 0, 0);
                 }
+                KeepInScreen();
                 // 每隔5分钟移动鼠标
                 Thread.Sleep(TimeSpan.FromMinutes(5));
                 i++;
             }
+        }
+
+        private static void KeepInScreen()
+        {
+            var screenSize = Screenshot.GetScreenPhysicalSzie();
+            var arrowPoint = new Point(0, 0);
+            Win32Utils.GetCursorPos(ref arrowPoint);
+            // 超出边界则回到屏幕中心
+            if (arrowPoint.X > 10 && arrowPoint.X <= screenSize.Width - 10)
+            {
+                return;
+            }
+            else
+            {
+                arrowPoint.X = screenSize.Width / 2;
+            }
+            Win32Utils.MouseEvent(Win32Constants.MOUSEEVENTF_MOVE, screenSize.Width / 2, 0, 0, 0);
         }
     }
 }
