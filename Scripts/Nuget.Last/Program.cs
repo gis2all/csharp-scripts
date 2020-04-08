42424a40729a139615f0dfa8a51f195aee1ac245
Chao Wang <cwang@esri.com>
2020-04-08 10:48:38 +0800

    Add code

---
+++ Scripts/Nuget.Last/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nuget.Last
+{
+    // 使用DotNet as script插件可以在Jenkins使用C#脚本
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var file = @"E:\Applications\DotNet\WinDesktop\Apps\arcgis-earth\source\Directory.Build.props";
+            var targetNugetDir = @"C:\Users\chao9441\.nuget\packages\esri.arcgisruntime.wpf\";
+            if (System.IO.File.Exists(file))
+            {
+                var text = System.IO.File.ReadAllText(file);
+                var splitStr = text.Split("Version=");
+                if (splitStr.Length == 2)
+                {
+                    // 截取DailyBuid version
+                    var dailyBuild = splitStr[1].Substring(1, 17);
+                    var currentDailyBuild = targetNugetDir + dailyBuild;
+                    // 如果不存在则删除整个文件夹，这样可以获取最新的Nuget包
+                    if (!System.IO.Directory.Exists(currentDailyBuild))
+                    {
+                        System.IO.Directory.Delete(targetNugetDir, true);
+                    }
+                }
+            }
+            else
+            {
+                System.IO.Directory.Delete(targetNugetDir, true);
+            }
+        }
+    }
+}
