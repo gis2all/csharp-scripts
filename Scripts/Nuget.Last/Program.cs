0b3fb6e5f91ac5a6592c4bef9bab1c8a5da2c48b
Chao Wang <cwang@esri.com>
2020-04-10 11:45:30 +0800

    Minor change

--- Scripts/Nuget.Last/Program.cs
+++ Scripts/Nuget.Last/Program.cs
@@ -11,9 +11,13 @@ namespace Nuget.Last
             // 替换成你自己的路径
             var file = @"E:\Applications\DotNet\WinDesktop\Apps\arcgis-earth\source\Directory.Build.props";
             var targetNugetDir = @"C:\Users\chao9441\.nuget\packages\esri.arcgisruntime.wpf\";
+            if (!Directory.Exists(targetNugetDir))
+            {
+                Directory.CreateDirectory(targetNugetDir);
+            }
             if (File.Exists(file))
             {
-                var text = System.IO.File.ReadAllText(file);
+                var text = File.ReadAllText(file);
                 var splitStr = text.Split("Version=");
                 if (splitStr.Length == 2)
                 {
@@ -21,11 +25,11 @@ namespace Nuget.Last
                     var dailyBuild = splitStr[1].Substring(1, 17);
                     var currentDailyBuild = targetNugetDir + dailyBuild;
                     // 如果不存在则删除整个文件夹，这样可以获取最新的Nuget包
-                    if (!System.IO.Directory.Exists(currentDailyBuild))
+                    if (!Directory.Exists(currentDailyBuild))
                     {
                         Directory.Delete(targetNugetDir, true);
                         Console.WriteLine($"Daily Build changes!");
-                        Console.WriteLine($"Delete nuget dir to get the last nuget package: {targetNugetDir}");
+                        Console.WriteLine($"Delete nuget dir to get the last nuget package: {currentDailyBuild}");
                     }
                     else
                     {
